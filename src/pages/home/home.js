2e73c88fa819184cdb9e76723f808b89bd1af918
onurat <114289826+onurat@users.noreply.github.com>
2023-11-01 22:27:19 +0000

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -10,8 +10,6 @@ function Home() {
 
       <div className='description_section_1'>
         <Description Description="At Supreme Autoglazing, we're your trusted partner for replacing and repairing broken glass on all types of vehicles in Greater London. We take pride in delivering top-notch workmanship and quality products to get you back on the road quickly and safely." />
-        <Description Description="At Supreme Autoglazing, we're your trusted partner for replacing broken glass on all types of vehicles in Greater London. We take pride in delivering top-notch workmanship and quality products to get you back on the road quickly and safely." />
-        <Description Description="At Supreme Autoglazing, we're your trusted partner for replacing broken glass on all types of vehicles in Greater London. We take pride in delivering top-notch workmanship and quality products to get you back on the road quickly and safely." />
       </div>
 
       <ImageButton title="Glass Replacment" url="/glassreplacment" image="\img\IMG_E4310.JPG" />
