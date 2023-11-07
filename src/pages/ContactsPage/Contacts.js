96c33c596bce5a2bbadcc78fe4423be3fc880af0
onurat <114289826+onurat@users.noreply.github.com>
2023-11-07 19:02:45 +0000

    .
    .

--- src/pages/ContactsPage/Contacts.js
+++ src/pages/ContactsPage/Contacts.js
@@ -1,12 +1,47 @@
 import React from "react";
 import "./Contacts.css";
 
-function Contacs() {
+function Contacts() {
   return (
-    <div>
-      <h1>Contacs Page</h1>
+    <div className="contacts-container">
+      <h1>Contact Us</h1>
+      <p>Feel free to reach out to us with any questions or feedback:</p>
+      <div className="contact-info">
+        <div className="contact-item">
+          <i className="fas fa-envelope"></i>
+          <span>Email: contact@example.com</span>
+        </div>
+        <div className="contact-item">
+          <i className="fas fa-phone"></i>
+          <span>Phone: +1 (123) 456-7890</span>
+        </div>
+        <div className="contact-item">
+          <i className="fas fa-map-marker-alt"></i>
+          <span>Address: 123 Main Street, City, Country</span>
+        </div>
+      </div>
+      <div className="contact-form">
+        <h2>Send us a message</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" rows="4"></textarea>
+          </div>
+          <button type="submit" className="submit-button">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default Contacs;
+export default Contacts;
