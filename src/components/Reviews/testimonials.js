611fb2c2101b7ecf336b187661d75f94cc08abda
onurat <114289826+onurat@users.noreply.github.com>
2023-11-08 17:44:39 +0000

    Review section added
    previous review added will add option for customers to add in future

---
+++ src/components/Reviews/testimonials.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Testimonials.css';
+
+function Testimonials({ testimonials }) {
+  return (
+    <div className="testimonials">
+      <h2 className="testimonials-title">Customer Testimonials</h2>
+      <div className="testimonial-list">
+        {testimonials.map((testimonial, index) => (
+          <div className="testimonial" key={index}>
+            <p className="testimonial-text">{testimonial.text}</p>
+            <p className="testimonial-author">{testimonial.author}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Testimonials;
