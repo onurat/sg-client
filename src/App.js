63b3d5feb928e514fc255278360e71c6dfb63f94
onurat <114289826+onurat@users.noreply.github.com>
2023-11-14 20:42:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import { routes } from './pages/index';
 
 function App() {
-  const basename = "/";
   return (
     <div className="App">
-    <Router basename={basename}>
-      <Navbar />
-      <Routes>
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </Router>
-    <Footer />
-  </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
