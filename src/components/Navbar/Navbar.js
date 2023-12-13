c744a9a0dc313604d7ea1d24acaa876c5564d9ef
onurat <114289826+onurat@users.noreply.github.com>
2023-12-13 15:41:47 +0000

    new updates to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,56 +1,23 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './Navbar.css';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const sidebarRef = useRef();
+import './Navbar.css';
 
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
-
-  const closeMenu = (e) => {
-    if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
-      setMenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', closeMenu);
-
-    return () => {
-      document.removeEventListener('mousedown', closeMenu);
-    };
-  }, []);
-
+const Navbar = () => {
   return (
-    <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
-      <img className="navbar-logo" src="/img/SA.jpg" alt="Supreme Autoglazing logo" />
-      <div className="navbar-logo-name">SUPREME AUTOGLAZING</div>
-      <div className="navbar-menu">
-        <div className="hamburger-menu" onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
+    <nav className="navbar">
+      <div className="navbar-content">
+        <div className="logo">
+          <img src="/img/SA.jpg" alt="supreme autoglazing" />
+          <h1>Supreme Autoglazing</h1>
         </div>
-        <div className={`navbar-button ${isMenuOpen ? 'open' : ''}`} ref={sidebarRef}>
-          <Link to="/" onClick={toggleMenu}>
-            Home
-          </Link>
-          <Link to="/contacts" onClick={toggleMenu}>
-            Contact
-          </Link>
-          <Link to="/windscreenrepair" onClick={toggleMenu}>
-            Windscreen Repair
-          </Link>
-          <Link to="/glassreplacment" onClick={toggleMenu}>
-            Glass Replacement
-          </Link>
+        <div className="nav-buttons">
+          <Link to="/">Home</Link>
+          <Link to="/Contacts">Contact</Link>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
